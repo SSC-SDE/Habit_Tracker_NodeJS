1b4c8a4a1765b1e22e8629d5e15aaeb165a03af4
SSC-SDE <sobaiamakechene@gmail.com>
2024-04-14 21:53:08 +0530

    try

--- config/mongoose.js
+++ config/mongoose.js
@@ -15,9 +15,9 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+  } catch{
+    console.error("failed");
   }
+  
 }
 run().catch(console.dir);
