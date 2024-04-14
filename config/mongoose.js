da85dc7baf1fb6b6f82fddb9abbbd852178719ca
SSC-SDE <sobaiamakechene@gmail.com>
2024-04-14 22:03:42 +0530

    new ids

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://shankhyac:<password>@clustertestforapp.raezrtd.mongodb.net/?retryWrites=true&w=majority&appName=ClusterTestforApp";
+const uri = "mongodb+srv://shankhyac:a9883056609@clustertestforapp.raezrtd.mongodb.net/?retryWrites=true&w=majority&appName=ClusterTestforApp";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
